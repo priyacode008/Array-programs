9a24a69c7ba70deb5ffcab9780c73b8248502fc0
priyacode008 <priya.software008@gmail.com>
2026-07-10 18:57:32 +0530

    Create rightrotate.java

---
+++ rightrotate.java
@@ -0,0 +1,18 @@
+public class rightrotate{
+    public static void main(String args []){
+        int a[]={1,2,3,4,5};
+
+        int k=2;
+
+        for(int j=0;j<k;j++){
+            int last=a[a.length-1];
+            for(int i=a.length-1;i>0;i--){
+                a[i]=a[i-1];
+            }
+            a[0]=last;
+        }
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
